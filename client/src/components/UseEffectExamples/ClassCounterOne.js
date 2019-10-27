3455b4184ebaff3d6c0b65ad143e9c55f4949d19
itsevalieu <itsevalieu@gmail.com>
2019-10-27 15:07:09 -0700

    Include example of conditional rerender for useEffect

--- client/src/components/UseEffectExamples/ClassCounterOne.js
+++ client/src/components/UseEffectExamples/ClassCounterOne.js
@@ -4,7 +4,8 @@ export default class ClassCounterOne extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0
+            count: 0,
+            name: ''
         }
     }
 
@@ -12,13 +13,21 @@ export default class ClassCounterOne extends Component {
         document.title = `Clicked ${this.state.count} times`;
     }
     componentDidUpdate(prevProps, prevState) {
-        document.title = `Clicked ${this.state.count} times`;
+        if (prevState.count !== this.state.count) {
+            console.log('Updating document title');
+            document.title = `Clicked ${this.state.count} times`;
+        }
     }
     render() {
         return (
             <div>
+                <input
+                    type='text'
+                    value={this.state.name}
+                    onChange={e => { this.setState({ name: e.target.value }) }}
+                />
                 <button onClick={() => this.setState({ count: this.state.count + 1 })}> Click {this.state.count} times </button>
-            </div>
+            </div >
         );
     }
 }
--- client/src/components/UseEffectExamples/EffectHookCounterOne.js
+++ client/src/components/UseEffectExamples/EffectHookCounterOne.js
@@ -2,12 +2,22 @@ import React, { useState, useEffect } from 'react';
 
 export default function EffectHookCounterOne() {
     const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
 
-    useEffect(() => { document.title = `You clicked ${count} times` });
+    useEffect(() => {
+        console.log('useEffect - document title updated');
+        document.title = `You clicked ${count} times`;
+    }, [count]); //useEffect has a second parameter, pass an array of the state that useEffect is conditional on to update on rerender
+
     return (
         <div>
+            <input
+                type='text'
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />
             <button onClick={() => setCount(count + 1)}>Click {count} times</button>
-        </div>
+        </div >
     );
 }
 
