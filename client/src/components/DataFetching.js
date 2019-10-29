a678d75f4e6fb052b182dbadfe362f3495e165a1
itsevalieu <itsevalieu@gmail.com>
2019-10-28 19:55:44 -0700

    Adding Button to handleClick fetch post

--- client/src/components/DataFetching.js
+++ client/src/components/DataFetching.js
@@ -5,6 +5,7 @@ export default function DataFetching() {
     const [posts, setPosts] = useState([]);
     const [post, setPost] = useState({});
     const [id, setId] = useState(1);
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1);
 
     useEffect(() => {
         axios('https://jsonplaceholder.typicode.com/posts')
@@ -18,7 +19,7 @@ export default function DataFetching() {
     }, []);
 
     useEffect(() => {
-        axios(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        axios(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
             .then(res => {
                 console.log(res.data);
                 setPost(res.data);
@@ -26,8 +27,11 @@ export default function DataFetching() {
             .catch(err => {
                 console.log(err);
             });
-    }, [id]);
+    }, [idFromButtonClick]);
 
+    const handleClick = () => {
+        setIdFromButtonClick(id);
+    }
     return (
         <div>
             <input
@@ -35,6 +39,7 @@ export default function DataFetching() {
                 value={id}
                 onChange={e => setId(e.target.value)}
             />
+            <button onClick={handleClick}>Fetch Post</button>
             <p>{post.title}</p>
             <ul>
                 {posts.map(post => <li key={post.id}>{post.title}</li>)}
